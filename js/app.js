720df356e7f79658fc16de9049a61682dcdc2e57
Danijel Buhin <danijelbuhin6@gmail.com>
2018-02-19 20:18:47 +0100

    create home banner

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import main from '../scss/main.scss';
+
+import * as $ from 'jquery';
+
+particlesJS.load('banner-particles-center', './banner-particles-center.json');
+particlesJS.load('banner-particles-left', './banner-particles-left.json');
+particlesJS.load('banner-particles-right', './banner-particles-right.json');
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'main.css',
+});
+
+const config = {
+  entry: './js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: ['css-loader', 'sass-loader'],
+          fallback: 'style-loader',
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    extractSass,
+  ],
+};
+
+module.exports = config;
