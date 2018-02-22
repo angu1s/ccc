9faf62eac73b72012b9834355b6b06d72526403d
Danijel Buhin <danijelbuhin6@gmail.com>
2018-02-22 20:53:22 +0100

    Landing done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
-import main from '../scss/main.scss';
-
-import * as $ from 'jquery';
+import style from '../scss/main.scss';
 
 particlesJS.load('banner-particles-center', './banner-particles-center.json');
 particlesJS.load('banner-particles-left', './banner-particles-left.json');
 particlesJS.load('banner-particles-right', './banner-particles-right.json');
+
+$('.nav-show').on('click', function() {
+  $('body').css('overflow-y', 'hidden');
+  $('nav').addClass('fixed');
+});
+
+$('.nav-close').on('click', function () {
+  $('body').css('overflow-y', 'visible');
+  $('nav').removeClass('fixed');
+});
+
+$('nav a').on('click', function () {
+  if ($('nav').hasClass('fixed')) {
+    $('nav').removeClass('fixed');
+    $('body').css('overflow-y', 'visible');
+  }
+  var hash = $(this).attr('href');
+  console.log(hash);
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800);
+});
